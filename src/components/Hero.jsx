cad2619e875cbba9096daeae48575ffa914259e4
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-06 14:05:30 +0300

    Refactor Hero component layout and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,49 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
+import styles from "../style";
 
 const Hero = () => {
   return (
-    <section id="home" className={` ${styles.paddingY}`}>
-      <div className={`${styles.flexStart} `}>
-        <div className="">
-          <img src={discount} alt="discount" className="" />
-          <p className={`${styles.paragraph} ml-2`}>
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+      >
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p
+            className={`font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2`}
+          >
             <span className="text-white">20%</span> Discount For{" "}
             <span className="text-white">1 Month</span> Account
           </p>
         </div>
-
-        <div className="">
-          <h1 className=" ">
-            The Next <br className="" />{" "}
+        <div className="bg-neutral-600   flex  flex-row justify-between items-center w-full ">
+          <h1 className=" flex-1 font-poppins  font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+            The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "}
           </h1>
-          <div className="ss:flex ">
+          <div className="ss:flex hidden md:mr-4 mr-0 ">
             <GetStarted />
           </div>
         </div>
-
-        <h1 className=" ">Payment Method.</h1>
-        <p className={`${styles.paragraph}  `}>
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          Payment Method.
+        </h1>{" "}
+        <p
+          className={`font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]   max-w-[470px]  mt-5 `}
+        >
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
           annual fees.
         </p>
       </div>
 
-      <div className={`${styles.flexCenter} `}>
+      <div className={`flex justify-center items-center `}>
         <img src={robot} alt="billing" className="w-[100%] h-[100%]  z-[5]" />
 
         {/* gradient start */}
@@ -44,7 +53,7 @@ const Hero = () => {
         {/* gradient end */}
       </div>
 
-      <div className={` ${styles.flexCenter}`}>
+      <div className={` flex justify-center items-center`}>
         <GetStarted />
       </div>
     </section>
