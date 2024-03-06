5503939e2456feb601873adcd1c28fbe9c88348a
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-06 20:16:46 +0300

    Add new CTA section to NewCta component

--- src/components/NewCta.jsx
+++ src/components/NewCta.jsx
@@ -1,8 +1,36 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
 const NewCta = () => {
-  return <div>NewCta</div>;
+  return (
+    <section
+      className={`   flex justify-center items-center ${styles.marginY} ${styles.padding} 
+      sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <div className=" flex-1 flex flex-col">
+        <h2
+          className="font-poppins font-semibold xs:text-[48px] text-[40px] text-white 
+        xs:leading-[76.8px] leading-[66.8px] w-full"
+        >
+          Let’s try our service now!
+        </h2>
+        <p
+          className={` font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5`}
+        >
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+
+      <div
+        className={` flex justify-center items-center sm:ml-10 ml-0 sm:mt-0 mt-10`}
+      >
+        <Button />
+      </div>
+    </section>
+  );
 };
 
 export default NewCta;
