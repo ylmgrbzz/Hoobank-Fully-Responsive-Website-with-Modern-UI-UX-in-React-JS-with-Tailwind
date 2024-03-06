76782ec2ba7e5174689b97bbff7c00986f663112
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-06 19:56:03 +0300

    Refactor Billing component and style.js

--- src/style.js
+++ src/style.js
@@ -18,13 +18,13 @@ const styles = {
 };
 
 export const layout = {
-  section: `flex md:flex-row flex-col ${styles.paddingY}`,
-  sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+  section: `flex md:flex-row flex-col sm:py-16 py-6`,
+  sectionReverse: `flex md:flex-row flex-col-reverse sm:py-16 py-6`,
 
-  sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
-  sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+  sectionImgReverse: `flex-1 flex flex justify-center items-center md:mr-10 mr-0 md:mt-0 mt-10 relative`,
+  sectionImg: `flex-1 flex flex justify-center items-center md:ml-10 ml-0 md:mt-0 mt-10 relative`,
 
-  sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
+  sectionInfo: `flex-1 flex justify-center items-start flex-col`,
 };
 
 export default styles;
