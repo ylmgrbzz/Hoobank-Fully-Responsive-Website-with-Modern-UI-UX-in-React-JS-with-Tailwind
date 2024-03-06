c2797fbb7f3f5e377f791d0e95a4bde37b751649
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-06 14:15:17 +0300

    Refactor GetStarted component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,19 +1,27 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import styles from "../style";
 import { arrowUp } from "../assets";
+import styles from "../style";
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} `}>
-    <div className={`${styles.flexCenter} `}>
-      <div className={`${styles.flexStart} `}>
-        <p className=" ">
-          <span className="">Get</span>
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}
+  >
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    >
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className=" font-poppins font-medium text-[18px] leading-[23.4px]">
+          <span className="text-gradient">Get</span>
         </p>
-        <img src={arrowUp} alt="arrow-up" className="" />
+        <img
+          src={arrowUp}
+          alt="arrow-up"
+          className="w-[23px] h-[23px] object-contain"
+        />
       </div>
 
-      <p className="">
+      <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
         <span className="text-gradient">Started</span>
       </p>
     </div>
